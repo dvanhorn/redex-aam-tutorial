449731bcf99c1679e75d898093dec219cc59b77e
David Van Horn <dvanhorn@ccs.neu.edu>
2014-08-05 16:22:06 -0400

    Make subst more concise for readability.

--- subst.rkt
+++ subst.rkt
@@ -1,60 +1,53 @@
 #lang racket
 (require redex/reduction-semantics)
 (provide (all-defined-out))
+
 (define-language L
-  (T num)
+  (T any)
+  (M any)
   (X (variable-except λ μ if0 : num)))
   
 (define-metafunction L
-  subst : (X any) ... any -> any 
-  [(subst (X_1 any_1) (X_2 any_2) ... any_3)
-   (subst-1 X_1 any_1 (subst (X_2 any_2) ... any_3))]
-  [(subst any_3) any_3])
+  subst : (X M) ... M -> M
+  [(subst (X_1 M_1) (X_2 M_2) ... M_3)
+   (subst-1 X_1 M_1 (subst (X_2 M_2) ... M_3))]
+  [(subst M_3) M_3])
 
 (define-metafunction L
-  subst-1 : X any any -> any 
-  ;; 1. x_1 bound, so don't continue in λ body 
-  [(subst-1 X_1 any_1 (λ ([X_2 : T_2] ... [X_1 : T_1] [X_3 : T_3] ...) any_2))
-   (λ ([X_2 : T_2] ... [X_1 : T_1] [X_3 : T_3] ...) any_2)
+  subst-1 : X M M -> M
+  ;; 1. X_1 bound, so don't continue in λ body
+  [(subst-1 X_1 M_1 (λ ([X_2 : T_2] ... [X_1 : T_1] [X_3 : T_3] ...) M_2))
+   (λ ([X_2 : T_2] ... [X_1 : T_1] [X_3 : T_3] ...) M_2)
    (side-condition (not (member (term X_1) (term (X_2 ...)))))]
   ;; or μ
-  [(subst-1 X any_1 (μ (X : T) any_2))
-   (μ (X : T) any_2)]
+  [(subst-1 X M_1 (μ (X : T) M_2))
+   (μ (X : T) M_2)]
   
-  ;; 2. general purpose capture avoiding case 
-  [(subst-1 X_1 any_1 (λ ([X_2 : T_2] ...) any_2)) 
-   (λ ([X_new : T_2] ...) (subst-1 X_1 any_1 (subst-vars (X_2 X_new) ... any_2)))
-   (where (X_new ...) ,(variables-not-in (term (X_1 any_1 any_2)) (term (X_2 ...))))]
+  ;; 2. general purpose capture avoiding case
+  [(subst-1 X_1 M_1 (λ ([X_2 : T_2] ...) M_2)) 
+   (λ ([X_new : T_2] ...) (subst-1 X_1 M_1 (subst-vars (X_2 X_new) ... M_2)))
+   (where (X_new ...) ,(variables-not-in (term (X_1 M_1 M_2)) (term (X_2 ...))))]
   ;; and μ
-  [(subst-1 X_1 any_1 (μ (X_2 : T) any_2))
-   (μ (X_new : T) (subst-1 X_1 any_1 (subst-vars (X_2 X_new) any_2)))
-   (where (X_new) ,(variables-not-in (term (X_1 any_1 any_2)) (term (X_2))))]
+  [(subst-1 X_1 M_1 (μ (X_2 : T) M_2))
+   (μ (X_new : T) (subst-1 X_1 M_1 (subst-vars (X_2 X_new) M_2)))
+   (where (X_new) ,(variables-not-in (term (X_1 M_1 M_2)) (term (X_2))))]
    
-  ;; 3. replace x_1 with e_1 
-  [(subst-1 X_1 any_1 X_1) any_1]
-  ;; 4. x_1 and x_2 are different, so don't replace [(subst x_1 any_1 x_2) x_2] 
+  ;; 3. replace X_1 with M_1
+  [(subst-1 X_1 M_1 X_1) M_1]
+  ;; 4. X_1 and X_2 are different, so don't replace
+  [(subst-1 X_1 M_1 X_2) X_2]
   ;; the last cases cover all other expressions
-  [(subst-1 X_1 any_1 (any_2 ...)) ((subst-1 X_1 any_1 any_2) ...)] 
-  [(subst-1 X_1 any_1 any_2) any_2])
+  [(subst-1 X_1 M_1 (M_2 ...)) ((subst-1 X_1 M_1 M_2) ...)] 
+  [(subst-1 X_1 M_1 M_2) M_2])
 
 (define-metafunction L 
-  subst-vars : (X any) ... any -> any
-  [(subst-vars (X_1 any_1) X_1) any_1] 
-  [(subst-vars (X_1 any_1) (any_2 ...)) ((subst-vars (X_1 any_1) any_2) ...)]
-  [(subst-vars (X_1 any_1) any_2) any_2]
-  [(subst-vars (X_1 any_1) (X_2 any_2) ... any_3) (subst-vars (X_1 any_1) (subst-vars (X_2 any_2) ... any_3))] 
-  [(subst-vars any) any])
+  subst-vars : (X M) ... M -> M
+  [(subst-vars (X_1 M_1) X_1) M_1] 
+  [(subst-vars (X_1 M_1) (M_2 ...)) 
+   ((subst-vars (X_1 M_1) M_2) ...)]
+  [(subst-vars (X_1 M_1) M_2) M_2]
+  [(subst-vars (X_1 M_1) (X_2 M_2) ... M_3) 
+   (subst-vars (X_1 M_1) (subst-vars (X_2 M_2) ... M_3))]
+  [(subst-vars M) M])
   
-
-(define-metafunction L
-  lookup : (any ...) any -> any
-  [(lookup ((any_k any_v) any ...) any_k) any_v]
-  [(lookup (any_0 any ...) any_k)
-   (lookup (any ...) any_k)])
-
-(define-metafunction L
-  :: : (any ...) any ... -> any
-  [(:: (any_1 ...) any_0 ...)
-   (any_0 ... any_1 ...)])
-
    
