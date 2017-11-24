1a392e8a5130525bc6fa57c871e0ccf59b2c2eb3
David Van Horn <dvanhorn@cs.umd.edu>
2017-11-24 13:20:01 -0500

    All your base.

--- bib.rkt
+++ bib.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang racket/base
 (require scriblib/autobib)
 (provide (all-defined-out))
 
--- ex.rkt
+++ ex.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang racket/base
 
 (require scribble/manual
          scribble/core
