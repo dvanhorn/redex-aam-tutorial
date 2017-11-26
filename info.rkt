26b15183c827fe2390fdab7aa692548c3637bfb0
David Van Horn <dvanhorn@cs.umd.edu>
2017-11-26 01:04:13 -0500

    Add scribblings to info.

--- info.rkt
+++ info.rkt
@@ -1,3 +1,4 @@
 #lang info
 (define deps (list "base" "redex-lib" "scheme-lib" "scribble-lib"))
 (define build-deps (list "redex-gui-lib" "sandbox-lib"))
+(define scribblings (list (list "tutorial.scrbl")))
